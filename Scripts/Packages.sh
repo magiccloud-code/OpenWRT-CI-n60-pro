3427e03694dd1153e6bddfb6df6ab4e37f97a2cb
magiccloud-code <magiccloud89@gmail.com>
2026-08-15 09:26:05 +0800

    Add clashoo package and enable luci-app-clashoo
    Enable luci-app-clashoo and register its updater. Updated Config/GENERAL.txt to disable luci-app-homeproxy and enable CONFIG_PACKAGE_luci-app-clashoo. Updated Scripts/Packages.sh to add UPDATE_PACKAGE entry for kenzok8/openwrt-clashoo (main, pkg).

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -63,6 +63,7 @@ UPDATE_PACKAGE "nikki" "nikkinikki-org/OpenWrt-nikki" "main"
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
 UPDATE_PACKAGE "passwall" "Openwrt-Passwall/openwrt-passwall" "main" "pkg"
 UPDATE_PACKAGE "passwall2" "Openwrt-Passwall/openwrt-passwall2" "main" "pkg"
+UPDATE_PACKAGE "clashoo" "kenzok8/openwrt-clashoo" "main" "pkg"
 
 #UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 
